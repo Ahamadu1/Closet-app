f0812526ceb20c9db658970118fd3efd5a4e4394
Ahamadu dukureh <ahamadudukureh1@gmail.com>
2025-09-03 20:21:17 -0400

    Adding all files

--- app/index.jsx
+++ app/index.jsx
@@ -1,31 +1,81 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
 import heartlogo from '../assets/hearticon.png';
 import stlyelogo from '../assets/hanger.png';
 import pluslogo from '../assets/plusbutton.png';
 import calendarlogo from '../assets/calendaricon.png';
-
+import Calendar from "./Calendar.jsx"
+import Favorites from "./Favorites.jsx"
+import Style from "./Style.jsx"
+import Add from "./Add.jsx"
+import { useRouter } from 'expo-router';
+const router = useRouter();
 
 const Home = () => {
     return (
       
         <LinearGradient
-        colors={['#1A0033', '#3D0066']} 
+        colors={['#2A003F', '#1A0029', '#0D0014']} 
+        locations={[0.1, 0.5, 1]}
+        start={{ x: 0.8, y: 0 }}
+          end={{ x: 1, y: 1 }}
         style={styles.container}>
-      <View>
+      {/* Full screen view */}
+      <View style={styles.container}>
       <Text style={styles.title}>Klozet</Text>
-        <View style={{flexDirection:"column"}}>
-    
-       
-        <Image source={heartlogo} style={styles.favorites}/>
-        </View>
+      
+
+      {/* Top Nav view */}
         <View style={{position: 'absolute',
-  left: 0,
-  right: 0,
-  bottom: 0,flexDirection:"column"}}>
+  left: 10,
+  right: 10,
+  top: 15,flexDirection:"column" }}>
+    
+     {/* Fits */}
+          <TouchableOpacity onPress={()=>router.push('/Fits')}>
+        <Text style={styles.fits}>Outfits</Text>
+        </TouchableOpacity>
+         
+         {/* Favorites */}
+         <TouchableOpacity onPress={()=>router.push('/Favorties')}>
+        <Image source={heartlogo} style={styles.favorites}/>
+        <Text style={{position:"absolute",right:1,fontSize: 14, top:33,          
+      fontWeight: 'medium',
+      color: 'white'}}>Favorites</Text>
+      </TouchableOpacity>
+        </View>
+        
+        {/* Top Border */}
+        <View style={{borderBottomColor:"grey",borderBottomWidth:1, marginVertical: 15 }}></View>
+       
+        {/* Bottom Border */}
+        <View style={{borderBottomColor:"grey",borderBottomWidth:1,marginVertical:670}}></View>
+
+        {/* Bottom Nav view */}
+        <View style={{position: 'absolute',
+  left: 10,
+  right: 10,
+  bottom: 70,flexDirection:"column", color:"white"}}>
+         
+         {/* Calendar */}
+         <TouchableOpacity onPress={()=>router.push('/Calendar')}>
         <Image source={calendarlogo} style={styles.calendar}/>
+        <Text style={{position:"absolute",left:1,fontSize: 14, top:38,          
+      fontWeight: 'medium', color: 'white'}}>Calendar</Text>
+        </TouchableOpacity>
+        
+        {/* Add */}
+        <TouchableOpacity onPress={()=>router.push('/Add')}>
         <Image source={pluslogo} style={styles.plus}/>
+        </TouchableOpacity>
+
+       {/* Style */} 
+       <TouchableOpacity onPress={()=>router.push('/Style')}>
+      <Text style={{position:"absolute",right:12,fontSize: 14, top:32,          
+      fontWeight: 'medium',
+      color: 'white'}}>Style</Text>
         <Image source={stlyelogo} style={styles.style}/>
+        </TouchableOpacity>
         </View>
 
         </View>
@@ -43,10 +93,13 @@ const styles = StyleSheet.create({container:{flex:1},
     color: 'white',
     marginTop: 10,
     alignSelf: "center",},
-    favorites:{height:50, width:50 ,position:"absolute",right:15,bottom:15},
-    style:{height:50, width:50 ,position:"absolute",right:15,marginBottom:100},
-    calendar:{height:50, width:50,position:"absolute", alignSelf: "flex-start",},
-    plus:{height:50, width:50 ,position:"absolute",marginBottom:10,marginBottom:5, alignSelf: "center"},
-    fits:{height:50, width:50 ,position:"absolute",marginLeft:5,marginTop:10}
+    
+    favorites:{height:30, width:35 ,position:"absolute",right:13,top:2},
+    style:{height:40, width:40 ,position:"absolute",right:10},
+    calendar:{height:40, width:40,position:"absolute", left:10, top:0},
+    plus:{height:40, width:40 ,position:"absolute", alignSelf: "center"},
+    fits:{position:"absolute",left:7,fontSize: 18, top:9,          
+      fontWeight: 'medium',
+      color: 'white',}
 
 })
